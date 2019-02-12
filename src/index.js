c18e8ac2d4b337304f719aa74b8634933a366e96
Marat <kashaevmt@gmail.com>
2019-02-13 01:03:09 +0300

    add routes to login and register

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
+import Login from './components/Auth/Login'
+import Register from './components/Auth/Register'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
+    </Switch>
+  </Router>
+)
+
+ReactDOM.render(<Root />, document.getElementById('root'))
 

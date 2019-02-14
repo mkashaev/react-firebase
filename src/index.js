ce1ed29f90261c82c4379f7728858b7c09b330ac
Marat <kashaevmt@gmail.com>
2019-02-14 12:50:16 +0300

    add redux to auth

--- src/index.js
+++ src/index.js
@@ -3,22 +3,54 @@ import ReactDOM from 'react-dom'
 import App from './components/App'
 import Login from './components/Auth/Login'
 import Register from './components/Auth/Register'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route, withRouter} from 'react-router-dom'
+import firebase from './firebase'
 
+import { createStore } from 'redux'
+import { Provider, connect } from 'react-redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import rootReducer from './reducers'
+import { setUser } from './actions'
+
+import Spiner from './Spiner'
 import 'semantic-ui-css/semantic.min.css'
 import './components/App.css'
 
+const store = createStore(rootReducer, composeWithDevTools())
 
+class Root extends React.Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.props.setUser(user)
+        this.props.history.push('/')
+      }
+    })
+  }
+  
+  render() {
+    return this.props.isLoading ? <Spiner /> : (
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    )
+  }
+}
 
-const Root = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={App} />
-      <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
-    </Switch>
-  </Router>
+const mapStateToProps = state => ({
+  isLoading: state.user.isLoading
+})
+
+const RootWithAuth = withRouter(connect(mapStateToProps, { setUser })(Root))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <RootWithAuth/>
+    </Router>
+  </Provider>,
+  document.getElementById('root')
 )
-
-ReactDOM.render(<Root />, document.getElementById('root'))
 
